d34c3868aa6622f0ccec01fd2c778f26d4b68dde
mert-erdem <47994087+mert-erdem@users.noreply.github.com>
2019-05-27 23:26:12 +0300

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje22019
+{
+    //MUSTERI VE YONETICI CONTROLLER LARI FARKLI!!!!EK OLARAK VIEW SINIFLARIDA FARKLI CUNKU 
+    class ControllerMusteri 
+    {
+        private GorunumMusteri view { get; set; }
+
+
+        public ControllerMusteri(GorunumMusteri gm)
+        {
+            this.view = gm;
+        }
+
+        public void KayitOl(Musteri m)//Tamamlanmadı
+        {
+            string line;
+            FileStream akis;
+            StreamReader Okuma;
+
+            string Yol = "Oteller.txt";
+            akis = new FileStream(Yol, FileMode.Open, FileAccess.Read);
+            Okuma = new StreamReader(akis);
+
+            line = Okuma.ReadLine();
+
+            while (line != null)
+            {
+                line = Okuma.ReadLine();
+            }
+        }
+        public void Rezervasyonlarim(string ID)
+        {
+            this.view.KayitliRezervasyonlar(ID);
+        }
+
+        public void RezervasyonYap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UygunOteller(string sehir, string yildizSayisi,List<Otel> oteller)
+        {
+            
+            List<Otel> uygun = new List<Otel>();
+
+            
+
+            foreach(Otel o in oteller)
+            {
+                if(o.Sehir==sehir && o.YildizSayisi==double.Parse(yildizSayisi))
+                {
+                    uygun.Add(o);
+                }
+            }
+            if(uygun.Count==0)
+            {
+                Console.WriteLine("Aradiginiz kriterlerde otel bulunamadi");
+            }
+            else{
+                this.view.UygunOtelleriGoster(uygun);
+            }
+            
+
+            
+        }
+    }
+
+    class ControllerYonetici
+    {
+        private GorunumYonetici view { get; set; }
+
+
+        public ControllerYonetici(GorunumYonetici gy)
+        {
+            this.view = gy;
+        }
+
+
+        public void OtelEkle()
+        {
+
+        }
+        public void OteleOdaEkle(Otel o)
+        {
+
+        }
+        public void GenelDurum(List<Musteri> musteriler)
+        {
+            
+
+            this.view.KayitliMusteriler(musteriler);
+            this.view.VarolanOtelleriGoster();
+            this.view.Yoneticiler();
+
+        }
+    }
+
+    
+}
---
+++ View.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje22019
+{
+    //IKI FARKLI VIEW VAR
+    class GorunumYonetici
+    {
+        public void KayitliMusteriler(List<Musteri> musteriler)
+        {
+            
+
+            
+            foreach(Musteri m in musteriler)
+            {
+                Console.WriteLine(m.Ad + " " + m.Soyad + " " + m.ID);
+            }
+        }
+
+        public void VarolanOtelleriGoster()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Yoneticiler()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+    class GorunumMusteri
+    {
+        public void UygunOtelleriGoster(List<Otel> o)
+        {
+            foreach (Otel otel in o)
+            {
+                if(otel is Hotel)
+                {
+                    Console.WriteLine(otel.OtelAdi + " " + otel.YildizSayisi + " " + otel.Sehir + " " +"(Hotel)");
+                }
+                if (otel is TatilKoyu)
+                {
+                    Console.WriteLine(otel.OtelAdi + " " + otel.YildizSayisi + " " + otel.Sehir + " " + "(TatilKoyu)");
+                }
+                if (otel is Pansiyon)
+                {
+                    Console.WriteLine(otel.OtelAdi + " " + otel.YildizSayisi + " " + otel.Sehir + " " + "(Pansiyon)");
+                }
+
+            }
+        }
+
+        public void KayitliRezervasyonlar(string ID)
+        {
+            string line;
+            FileStream akis;
+            StreamReader Okuma;
+
+            string Yol = ID + ".txt";//Kisinin Rezervasyonlarının bulunduğu dosya
+            akis = new FileStream(Yol, FileMode.Open, FileAccess.Read);
+            Okuma = new StreamReader(akis);
+
+            line = Okuma.ReadLine();
+
+            while (line != null)
+            {
+                line = Okuma.ReadLine();
+            }
+        }
+    }
+}
